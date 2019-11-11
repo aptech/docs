0212b34e6c667b7c69e5bdcd914788f4ad1d2360
Matt Evans <1388610+matthewevans@users.noreply.github.com>
2019-11-11 12:11:57 -0700

    Specify TeX font for HTML-CSS output. Attempting to fix Safari 8.0 MathJax invisible HTML-CSS bug

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,12 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+mathjax_config = {
+    'extensions': ['tex2jax.js'],
+    'jax': ['input/TeX', 'output/HTML-CSS'],
+    'HTML-CSS': { fonts: ['TeX'] }
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
